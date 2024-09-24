188ed85ca03fce31903dc7675663b3bf9e384c23
Aditya Mittal <adityamittal2410@gmail.com>
2024-09-24 11:43:21 +0530

    add more designs

--- src/components/MyWork/GraphicDesign.jsx
+++ src/components/MyWork/GraphicDesign.jsx
@@ -14,13 +14,28 @@ import Design12 from "./GraphicDesigns/Design12.svg"
 import Design13 from "./GraphicDesigns/Design13.svg"
 import Design14 from "./GraphicDesigns/Design14.svg"
 import Design15 from "./GraphicDesigns/Design15.svg"
+import Design21 from "./GraphicDesigns/Design21.svg"
+import Design22 from "./GraphicDesigns/Design22.svg"
+import Design23 from "./GraphicDesigns/Design23.svg"
+import Design24 from "./GraphicDesigns/Design24.svg"
+import Design25 from "./GraphicDesigns/Design25.svg"
+import Design26 from "./GraphicDesigns/Design26.svg"
+import Design27 from "./GraphicDesigns/Design27.svg"
+import Design28 from "./GraphicDesigns/Design28.svg"
+
 
 function GraphicDesign() {
   const grid6x6 = [Design1, Design2, Design3, Design4, Design5, Design6, Design7, Design8, Design9, Design10, Design11, Design12, Design13, Design14, Design15]
+  const grid2x2 = [Design21, Design22, Design23, Design24, Design25, Design26, Design27, Design28]
   return (
-    <div>
-      <div className="grid md:gap-12 gap-y-10 md:grid-cols-3 bruno-ace my-12 md:px-12">
+    <div className='bruno-ace my-12 md:px-12'>
+      <div className="grid md:gap-12 gap-y-10 md:grid-cols-3">
         {grid6x6.map((design)=>{
+          return <img src={design} alt="" />
+        })}
+      </div>
+      <div className="grid md:gap-12 gap-y-10 md:grid-cols-2">
+        {grid2x2.map((design)=>{
           return <img src={design} alt="" />
         })}
       </div>
--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Navbar() {
   return (
-    <div className="navbar w-full max-w-[1600px] fixed top-0 bg-black bg-opacity-40 backdrop-blur-sm backdrop-opacity-70">
+    <div className="navbar w-full max-w-[1600px] absolute top-0 bg-black bg-opacity-40 backdrop-blur-sm backdrop-opacity-70">
       <div className="flex items-center justify-around py-4 text-[#D9D9D9]">
         <div className="logo flex w-auto">
           <p className="text-2xl md:text-3xl">Logo</p>
